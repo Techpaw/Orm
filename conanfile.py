a7a3774e3cd0de9b72603724a5640497695abfd9
Vlad Gramuzov <vlad.gramuzov@gmail.com>
2018-05-30 02:22:01 +0300

    chore(conan): Update conan receipt

--- conanfile.py
+++ conanfile.py
@@ -6,11 +6,13 @@ class OrmConan(ConanFile):
     license = "MIT"
     url = "https://github.com/Techpaw/Orm.git"
     description = "Experimental ORM for cpp projects (now supports Postgres only)"
-    settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
     default_options = "shared=False"
     generators = "cmake"
     exports_sources = "include/*"
+    no_copy_source = True
+    requires = "boost_lexical_cast/1.65.1@bincrafters/stable", \
+               "libpqxx/6.2.0@trigger-happy/testing"
 
     def source(self):
         self.run("git clone https://github.com/Techpaw/Orm.git")
@@ -19,5 +21,5 @@ class OrmConan(ConanFile):
     def package(self):
         self.copy("*.hpp", dst="include", src="orm/include")
 
-    def package_info(self):
-        self.cpp_info.libs = ["orm"]
+    def package_id(self):
+        self.info.header_only()
